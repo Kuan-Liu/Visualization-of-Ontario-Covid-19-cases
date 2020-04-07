bb814fa0f96b35dc0b8f12a75b5a3008d0a767ba
Kuan Liu <45974798+Kuan-Liu@users.noreply.github.com>
2020-04-07 03:41:06 -0400

    Add files via upload

---
+++ Pyramid_plot_demographic_Ontario.R
@@ -0,0 +1,93 @@
+
+#pyramid plot ontario cases
+library(tidyverse)
+library(ggpol)
+library(dplyr)
+
+on_case <- read.table("https://data.ontario.ca/dataset/f4112442-bdc8-45d2-be3c-12efae72fb27/resource/455fd63b-603d-4608-8216-7d8647f43350/download/conposcovidloc.csv", 
+                 header = TRUE,
+                 sep = ",")
+
+#keep data with only two sex groups and non-missing age;
+table(on_case$CLIENT_GENDER)
+# FEMALE        MALE TRANSGENDER     UNKNOWN 
+# 1421        1355           1          16 
+table(on_case$Age_Group)
+# <20     20s     30s     40s     50s     60s     70s     80s     90s Unknown 
+# 68     388     438     466     529     459     257     131      55       2 
+
+#regrouping age group, clapsing 80s and 90s;
+on_case_clean<-on_case
+levels(on_case_clean$Age_Group)<-ifelse(levels(on_case_clean$Age_Group) %in% c("80s","90s"),">80",levels(on_case_clean$Age_Group))
+
+on_case_clean$CLIENT_GENDER[on_case_clean$CLIENT_GENDER == "UNKNOWN"]<-NA
+on_case_clean$CLIENT_GENDER[on_case_clean$CLIENT_GENDER == "TRANSGENDER"]<-NA
+on_case_clean$Age_Group[on_case_clean$Age_Group == "Unknown"]<-NA
+
+
+on_case_clean<-on_case_clean[complete.cases(on_case_clean),] #2793-2774 = 19;
+on_case_resolve<-subset(on_case_clean, RESOLVED == "Yes") #828;
+
+#19 unknown age or gender, 4 out of 19 resolved;
+
+# 1.Create Dataset;
+case <- on_case_clean %>% group_by(Age_Group,CLIENT_GENDER) %>% summarise(Freq=n())
+case_resolve<- on_case_resolve %>% group_by(Age_Group,CLIENT_GENDER) %>% summarise(Freq=n())
+
+case$Freq[case$CLIENT_GENDER=="FEMALE"]<- (-case$Freq[case$CLIENT_GENDER=="FEMALE"])
+case_resolve$Freq[case_resolve$CLIENT_GENDER=="FEMALE"]<- (-case_resolve$Freq[case_resolve$CLIENT_GENDER=="FEMALE"])
+
+names(case)<-c("Age group","Gender","Freqency count (n)")
+names(case_resolve)<-c("Age group","Gender","Freqency count (n)")
+
+
+dfcase <- tibble(
+  Gender = case$Gender,
+  Data =  case$`Freqency count (n)`,
+  Agegp = case$`Age group`
+)
+
+
+dfresolve <- tibble(
+  Gender = case_resolve$Gender,
+  Data =  case_resolve$`Freqency count (n)`,
+  Agegp = case_resolve$`Age group`
+)
+
+png("covid19_demo_on_pyramid.png", width = 9, height = 8, units = "in", res = 600)
+
+casep<-ggplot(dfcase, aes(x = forcats::fct_rev(Agegp), y = Data, fill = Gender)) +
+  geom_bar(stat = "identity", position = position_dodge(width=-0.5), width = 0.8, colour="black") +
+  geom_text(size = 3, stat = 'identity',
+            aes(label=c(abs(Data)),
+  hjust=c(rep(c(-0.4,1.4),8)))) +
+  facet_share(~Gender, dir = "h", scales = "free", reverse_num = TRUE) +
+  coord_flip() + 
+  labs(y = paste("Frequency count (n)", " \n Total number of confirmed cases ",sum(abs(dfcase$Data))), x = "", title = paste("Demographic of Ontario COVID-19 confirm cases, as of", format(Sys.time(), "%b %d, %Y"), "\n (known age and gender in female and male)")) +
+  scale_fill_manual(values = c("orange", "lightblue"))+
+  theme(panel.background = element_rect(fill = "white", colour = "white"),
+        panel.grid.major = element_line(colour = "grey95"),
+        panel.grid.minor = element_line(colour = "grey95"), 
+        legend.position = "none",
+        axis.title = element_text(size=11),
+        plot.margin = margin(1,2, 1, 1, "cm"))
+
+resolvep<-ggplot(dfresolve, aes(x = forcats::fct_rev(Agegp), y = Data, fill = Gender)) +
+  geom_bar(stat = "identity", position = position_dodge(width=-0.5), width = 0.8, colour="black") +
+  geom_text(size = 3, stat = 'identity',
+            aes(label=c(abs(Data)),
+                hjust=c(rep(c(-0.4,1.4),8)))) +
+  facet_share(~Gender, dir = "h", scales = "free", reverse_num = TRUE) +
+  coord_flip() + 
+  labs(y = paste("Frequency count (n)", " \n Total number of resolved cases ",sum(abs(dfresolve$Data))), x = "", title = paste("Demographic of Ontario COVID-19 resolved cases, as of", format(Sys.time(), "%b %d, %Y"), "\n (known age and gender in female and male)")) +
+  scale_fill_manual(values = c("orange", "lightblue"))+
+  theme(panel.background = element_rect(fill = "white", colour = "white"),
+        panel.grid.major = element_line(colour = "grey95"),
+        panel.grid.minor = element_line(colour = "grey95"), 
+        legend.position = "none",
+        axis.title = element_text(size=11),
+        plot.margin = margin(1,2, 1, 1, "cm"))
+
+cowplot::plot_grid(casep, resolvep, nrow=2, rel_heights = c(0.5,0.5),align = "v")
+
+dev.off()
---
+++ covid19_heatmap_case.R
@@ -0,0 +1,62 @@
+
+#heatmap plot for ontario daily cases by PHU
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+library(reshape2)
+
+# Reference:
+# COVID-19 Canada Open Data Working Group. Epidemiological Data from the COVID-19 Outbreak in Canada. 
+# https://github.com/ishaberry/Covid19Canada. (Access Date).
+# I use the working groups cases.csv data set for generating the ontario daily case heatmap;
+
+# 1. Read spreadsheet;
+can_case <- read.table("C:/Users/kuan liu/Dropbox (UT_KLiu)/covid19/data/Covid19Canada-master/cases.csv", 
+                      header = TRUE, sep = ",")
+
+# 2. Create daily aggregate cases by reporting PHU city after march 5th;
+can_case$date_report<-as.Date(can_case$date_report, format="%d-%m-%Y")
+on_case <- filter(can_case, province == "Ontario" & date_report > "2020-03-05")  %>% group_by(date_report,health_region) %>% summarise(Freq=n())
+
+# 3. checking the daily case count distribution;
+summary(on_case$Freq)
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# 1.00    1.00    3.00   10.82   12.00  206.00 
+#I decide to break daily case numbers in the following groups, 0, 1-9, 10-49, 50-99, >100 (5 levels)
+
+# 4. create daily case groups, relabel in ggplot to 1-9, 10-49, 50-99 and 100+;
+on_case <- on_case %>%
+  mutate( cases = case_when(
+    Freq >=1 & Freq <=9    ~ "1",
+    Freq >=10 & Freq <=49  ~ "2",
+    Freq >=50 & Freq <=99  ~ "3",
+    Freq >=100 ~ "4"))
+
+# 5.1 reorder health region by case frequency count, note that toronto should have the highest cumulative frequncy;
+on_case_region<- filter(can_case, province == "Ontario" & date_report > "2020-03-05")  %>% group_by(health_region) %>% summarise(Freq=n())
+region_order <- unlist(on_case_region[rev(order(on_case_region$Freq)),][,1]) #save new region order by total case frequency to a vector;
+
+# 5.2 update original level to the new ordered regional level;
+on_case$health_region<- factor(on_case$health_region, levels=region_order[1:dim(on_case_region)[1]])
+
+
+# 6.plot;
+
+png("covid19_oncase_heatmap.png", width = 7.5, height = 8, units = "in", res = 400)
+
+ggplot(on_case, aes( health_region, as.factor(reorder(date_report, desc(date_report))))) +
+  coord_equal()+
+  scale_x_discrete(position = "top")+
+  geom_tile(aes(fill = cases )) + 
+  geom_text(aes(label = round(Freq, 1)), size=3) +
+  scale_fill_brewer(palette = "YlOrRd",label=c("1-9","10-49","50-99",expression(phantom(x) >=100))) +
+  labs(y = " ", x = "",fill="Daily cases",caption = "Heat Map of Ontario Daily Confirmed COVID-19 Cases by Region")+
+  theme(panel.background = element_rect(fill = "white", colour = "black"),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(), 
+      legend.position = "right",
+      axis.text.x = element_text(angle = 90, hjust = 0),
+      plot.margin = margin(0,0, 0, 0, "cm"), 
+      plot.caption = element_text(hjust=1, size=rel(1.2)))
+
+dev.off()
