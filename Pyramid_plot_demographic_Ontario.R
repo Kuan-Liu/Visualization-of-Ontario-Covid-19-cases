5c1508c6492d7b1731e28a29db9097ee00148ca2
Kuan Liu <45974798+Kuan-Liu@users.noreply.github.com>
2020-04-08 00:26:48 -0400

    Add files via upload
    Update code to match with the updated ontario spreadsheet

--- Pyramid_plot_demographic_Ontario.R
+++ Pyramid_plot_demographic_Ontario.R
@@ -9,37 +9,35 @@ on_case <- read.table("https://data.ontario.ca/dataset/f4112442-bdc8-45d2-be3c-1
                  sep = ",")
 
 #keep data with only two sex groups and non-missing age;
-table(on_case$CLIENT_GENDER)
-# FEMALE        MALE TRANSGENDER     UNKNOWN 
-# 1421        1355           1          16 
-table(on_case$Age_Group)
-# <20     20s     30s     40s     50s     60s     70s     80s     90s Unknown 
-# 68     388     438     466     529     459     257     131      55       2 
+# table(on_case$CLIENT_GENDER)
+# table(on_case$Age_Group)
+
 
 #regrouping age group, clapsing 80s and 90s;
 on_case_clean<-on_case
-levels(on_case_clean$Age_Group)<-ifelse(levels(on_case_clean$Age_Group) %in% c("80s","90s"),">80",levels(on_case_clean$Age_Group))
+# levels(on_case_clean$Age_Group)<-ifelse(levels(on_case_clean$Age_Group) %in% c("80s","90s"),">80",levels(on_case_clean$Age_Group))
 
-on_case_clean$CLIENT_GENDER[on_case_clean$CLIENT_GENDER == "UNKNOWN"]<-NA
-on_case_clean$CLIENT_GENDER[on_case_clean$CLIENT_GENDER == "TRANSGENDER"]<-NA
+on_case_clean$CLIENT_GENDER[on_case_clean$CLIENT_GENDER %in% c("UNKNOWN", "OTHER","TRANSGENDER")]<-NA
 on_case_clean$Age_Group[on_case_clean$Age_Group == "Unknown"]<-NA
 
 
-on_case_clean<-on_case_clean[complete.cases(on_case_clean),] #2793-2774 = 19;
-on_case_resolve<-subset(on_case_clean, RESOLVED == "Yes") #828;
+on_case_clean<-on_case_clean[complete.cases(on_case_clean),] 
+on_case_resolve<-subset(on_case_clean, OUTCOME1 == "Resolved") 
+on_case_death<-subset(on_case_clean, OUTCOME1 == "Fatal") 
 
-#19 unknown age or gender, 4 out of 19 resolved;
 
 # 1.Create Dataset;
 case <- on_case_clean %>% group_by(Age_Group,CLIENT_GENDER) %>% summarise(Freq=n())
 case_resolve<- on_case_resolve %>% group_by(Age_Group,CLIENT_GENDER) %>% summarise(Freq=n())
+case_death<- on_case_death %>% group_by(Age_Group,CLIENT_GENDER) %>% summarise(Freq=n())
 
 case$Freq[case$CLIENT_GENDER=="FEMALE"]<- (-case$Freq[case$CLIENT_GENDER=="FEMALE"])
 case_resolve$Freq[case_resolve$CLIENT_GENDER=="FEMALE"]<- (-case_resolve$Freq[case_resolve$CLIENT_GENDER=="FEMALE"])
+case_death$Freq[case_death$CLIENT_GENDER=="FEMALE"]<- (-case_death$Freq[case_death$CLIENT_GENDER=="FEMALE"])
 
 names(case)<-c("Age group","Gender","Freqency count (n)")
 names(case_resolve)<-c("Age group","Gender","Freqency count (n)")
-
+names(case_death)<-c("Age group","Gender","Freqency count (n)")
 
 dfcase <- tibble(
   Gender = case$Gender,
@@ -54,40 +52,64 @@ dfresolve <- tibble(
   Agegp = case_resolve$`Age group`
 )
 
-png("covid19_demo_on_pyramid.png", width = 9, height = 8, units = "in", res = 600)
+dfdeath <- tibble(
+  Gender = case_death$Gender,
+  Data =  case_death$`Freqency count (n)`,
+  Agegp = case_death$`Age group`
+)
+
+png("covid19_demo_on_pyramid.png", width = 13, height = 9, units = "in", res = 300)
 
 casep<-ggplot(dfcase, aes(x = forcats::fct_rev(Agegp), y = Data, fill = Gender)) +
   geom_bar(stat = "identity", position = position_dodge(width=-0.5), width = 0.8, colour="black") +
   geom_text(size = 3, stat = 'identity',
             aes(label=c(abs(Data)),
-  hjust=c(rep(c(-0.4,1.4),8)))) +
+  hjust=c(rep(c(-0.4,1.4),9)))) +
   facet_share(~Gender, dir = "h", scales = "free", reverse_num = TRUE) +
   coord_flip() + 
   labs(y = paste("Frequency count (n)", " \n Total number of confirmed cases ",sum(abs(dfcase$Data))), x = "", title = paste("Demographic of Ontario COVID-19 confirm cases, as of", format(Sys.time(), "%b %d, %Y"), "\n (known age and gender in female and male)")) +
-  scale_fill_manual(values = c("orange", "lightblue"))+
+  scale_fill_manual(values = c("pink", "lightblue"))+
   theme(panel.background = element_rect(fill = "white", colour = "white"),
         panel.grid.major = element_line(colour = "grey95"),
         panel.grid.minor = element_line(colour = "grey95"), 
         legend.position = "none",
         axis.title = element_text(size=11),
-        plot.margin = margin(1,2, 1, 1, "cm"))
+        plot.margin = margin(1,1, 0, 1, "cm"))
 
 resolvep<-ggplot(dfresolve, aes(x = forcats::fct_rev(Agegp), y = Data, fill = Gender)) +
   geom_bar(stat = "identity", position = position_dodge(width=-0.5), width = 0.8, colour="black") +
   geom_text(size = 3, stat = 'identity',
             aes(label=c(abs(Data)),
-                hjust=c(rep(c(-0.4,1.4),8)))) +
+                hjust=c(rep(c(-0.4,1.4),9)))) +
   facet_share(~Gender, dir = "h", scales = "free", reverse_num = TRUE) +
   coord_flip() + 
-  labs(y = paste("Frequency count (n)", " \n Total number of resolved cases ",sum(abs(dfresolve$Data))), x = "", title = paste("Demographic of Ontario COVID-19 resolved cases, as of", format(Sys.time(), "%b %d, %Y"), "\n (known age and gender in female and male)")) +
-  scale_fill_manual(values = c("orange", "lightblue"))+
+  labs(y = paste("Frequency count (n)", " \n Total number of resolved cases ",sum(abs(dfresolve$Data))), x = "", title = "Demographic of Ontario COVID-19 resolved cases") +
+  scale_fill_manual(values = c("pink", "lightblue"))+
   theme(panel.background = element_rect(fill = "white", colour = "white"),
         panel.grid.major = element_line(colour = "grey95"),
         panel.grid.minor = element_line(colour = "grey95"), 
         legend.position = "none",
         axis.title = element_text(size=11),
-        plot.margin = margin(1,2, 1, 1, "cm"))
+        plot.margin = margin(1,0, 0, 0, "cm"))
 
-cowplot::plot_grid(casep, resolvep, nrow=2, rel_heights = c(0.5,0.5),align = "v")
+dp<-ggplot(dfdeath, aes(x = forcats::fct_rev(Agegp), y = Data, fill = Gender)) +
+  geom_bar(stat = "identity", position = position_dodge(width=-0.5), width = 0.6, colour="black") +
+  geom_text(size = 3, stat = 'identity',
+            aes(label=c(abs(Data)),
+                hjust=c(rep(c(1.4,-0.4),5),1.4))) +
+  facet_share(~Gender, dir = "h", scales = "free", reverse_num = TRUE) +
+  coord_flip() + 
+  labs(y = paste("Frequency count (n)", " \n Total number of resolved cases ",sum(abs(dfresolve$Data))), x = "", title = "Demographic of Ontario COVID-19 fatal cases") +
+  scale_fill_manual(values = c("pink", "lightblue"))+
+  theme(panel.background = element_rect(fill = "white", colour = "white"),
+        panel.grid.major = element_line(colour = "grey95"),
+        panel.grid.minor = element_line(colour = "grey95"), 
+        legend.position = "none",
+        axis.title = element_text(size=11),
+        plot.margin = margin(1,1, 0, 0, "cm"))
+
+combo <- cowplot::plot_grid(resolvep, dp, rel_widths = c(0.47,0.53), align = "h")
+
+cowplot::plot_grid(casep, combo, nrow=2, rel_heights = c(0.5,0.5),align = "v")
 
 dev.off()
